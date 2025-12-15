c119213104d060a2614790d295110e01bbfe1e56
Abhinav65 <abinavijay63@gmail.com>
2025-12-15 22:38:31 +0530

    Second commit

--- train.py
+++ train.py
@@ -2,16 +2,21 @@ with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 print("length of dataset in characters: ", len(text))
 print(text[:1000])
-# here are all the unique characters that occur in this text
+
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print(''.join(chars))
 print(vocab_size)
-# create a mapping from characters to integers
+
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
 
 print(encode("hii there"))
 print(decode(encode("hii there")))
+
+import torch 
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000]) 
